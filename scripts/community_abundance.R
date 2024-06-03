d9ef883084c3086da41b714279a6099941543258
Jonathan Rondeau-Leclaire <97983060+jorondo1@users.noreply.github.com>
2024-06-03 13:02:55 -0400

    dynamic set default db

--- scripts/community_abundance.R
+++ scripts/community_abundance.R
@@ -4,8 +4,12 @@ library(tidyverse, phyloseq)
 source(url('https://raw.githubusercontent.com/jorondo1/misc_scripts/main/community_functions.R'))
 source(url('https://raw.githubusercontent.com/jorondo1/misc_scripts/main/rarefy_even_depth2.R'))
 
+# Import environment arguments
+args <- commandArgs(trailingOnly = TRUE)
+dbName <- args[1] #e.g. r214
+
 # Sample k-mer containment across databases 
-cntm.df <- read_delim("data/sourmash/cntm_sum.txt", 
+cntm.df <- read_delim("sourmash/cntm_sum.txt", 
                       col_names = c("Sample", "db", "cntm"))
 
 cntm.df %>% group_by(db) %>% 
@@ -21,7 +25,7 @@ ggplot(cntm.df, aes(x = db, y = cntm, fill=db)) +
 # Containment increase after adding MAGs 
 cntmDiff <- cntm.df %>% 
   pivot_wider(names_from = db, values_from = cntm) %>% 
-  mutate(fold_inc = custom/rs214)
+  mutate(fold_inc = custom/!!sym(dbName))
 
 cntmDiff %>% summarise(
   mean_inc = mean(fold_inc),
@@ -34,11 +38,11 @@ ggplot(cntmDiff, aes(x = "Boreal moss", y = fold_inc)) +
   labs(y = 'Fold increase in sample k-mers containment', x = 'Microbiome') +
   scale_y_continuous(limits = c(0, NA), breaks = seq(0, max(cntmDiff$fold_inc), by = 2))  # Set minimum y value and increments
 
-# Alpha diversity 
+# Alpha diversity #
 
 # Parse Sourmash gather output 
-custom <- parse_SM('data/sourmash/*custom_gather.csv')
-rs214 <- parse_SM('data/sourmash/*rs214_gather.csv')
+custom <- parse_SM('sourmash/*custom_gather.csv')
+GTDB <- parse_SM(paste0('sourmash/*',dbName,'_gather.csv'))
 
 # Import metadata from moss project 
 moss.ps <- readRDS(url("https://github.com/jorondo1/borealMoss/raw/main/data/R_out/mossMAGs.RDS",
@@ -49,12 +53,12 @@ sample_data <- moss_custom.ps@sam_data %>% data.frame %>% rownames_to_column("Sa
 moss_custom.ps <- phyloseq(otu_table(custom, taxa_are_rows = TRUE), 
                            sample_data(moss.ps@sam_data)) 
 
-moss_rs214.ps <- phyloseq(otu_table(rs214, taxa_are_rows = TRUE), 
+moss_GTDB.ps <- phyloseq(otu_table(GTDB, taxa_are_rows = TRUE), 
                           sample_data(moss.ps@sam_data)) 
 
 # compute alpha-diversity 
 moss_div = data.frame(
-  rs214 = rarefy_even_depth2(moss_rs214.ps) %>% 
+  GTDB = rarefy_even_depth2(moss_GTDB.ps) %>% 
     estimate_diversity(index = 'Shannon'),
   custom = rarefy_even_depth2(moss_custom.ps) %>% 
     estimate_diversity(index = 'Shannon')
@@ -64,7 +68,7 @@ moss_div_long <- moss_div %>%
   pivot_longer(
     values_to = 'Shannon',
     names_to = 'Database',
-    cols = c("rs214", "custom")) %>% 
+    cols = c(dbName, "custom")) %>% 
   left_join(dplyr::select(sample_data, Sample, Compartment, Host), 
             by = "Sample")
 
@@ -81,21 +85,21 @@ ggplot(moss_div_long, aes(x = Database, y = Shannon, fill = Host)) +
   facet_grid(~Database, scales = "free") + labs(x="") +
   geom_violin() + theme_light()
 
-lm_rs214 <- moss_div_long %>% 
-  filter(Database == 'rs214') %>% 
+lm_GTDB <- moss_div_long %>% 
+  filter(Database == dbName) %>% 
   lm(Shannon ~ Host, data = .) 
 
 lm_custom <- moss_div_long %>% 
   filter(Database == 'custom') %>% 
   lm(Shannon ~ Host, data = .) 
 
-aov(lm_rs214) %>% summary # barely significant difference
+aov(lm_GTDB) %>% summary # barely significant difference
 aov(lm_custom) %>% summary # highly significant difference
-summary(lm_rs214) %$% r.squared # 6% variance explained
+summary(lm_GTDB) %$% r.squared # 6% variance explained
 summary(lm_custom) %$% r.squared # 20% variance explained
 
 residuals(lm_custom) %>% shapiro.test # normal-ish p=0.04
-residuals(lm_rs214) %>% shapiro.test # normal p=0.50
+residuals(lm_GTDB) %>% shapiro.test # normal p=0.50
 
 # non-parametric supports
 moss_div_long %>% 
@@ -103,5 +107,5 @@ moss_div_long %>%
   kruskal.test(Shannon ~ Host, data = .)
 
 moss_div_long %>% 
-  filter(Database == 'rs214') %>% 
+  filter(Database == dbName) %>% 
   kruskal.test(Shannon ~ Host, data = .)
